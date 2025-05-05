dc8f7f018a4b43571f32ed4fda137d4f53ccd69f
zen <bucur_toma@yahoo.com>
2025-05-05 22:51:56 +0300

    updated test.py, clean up the docker-builder.yml, added new logic in docker.yml to verifing the branched

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,18 @@
-def test_homepage():
-    from app import app
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert b"Hello from your Dockerized Flask app" in response.data
+import pytest
+from app import app
 
-def test_database_connection():
-    from app import app
+# Use the Flask test client
+@pytest.fixture
+def client():
     with app.test_client() as client:
-        response = client.get('/db')
-        assert response.status_code == 200
-        assert b"Connected to PostgreSQL!" in response.data or b"Database connection failed" in response.data
+        yield client
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello from your Dockerized Flask app" in response.data
+
+def test_db_route(client):
+    response = client.get('/db')
+    assert response.status_code == 200
+    assert b"Connected to PostgreSQL" in response.data or b"Database connection failed" in response.data
